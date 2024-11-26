ddd4bcb14713473a6822478617ebfb4302d9b88b
David Santos <8220651@estg.ipp.pt>
2024-11-26 02:58:47 +0000

    sql funcional

--- setup_files/hosts.sh
+++ setup_files/hosts.sh
@@ -3,12 +3,15 @@
 # Variables with the IPs and names of the servers
 web1="192.168.51.121 web1"
 web2="192.168.51.122 web2"
+
 sql1="192.168.51.111 sql1"
 sql2="192.168.51.112 sql2"
+
 haproxy1_internal="192.168.51.100 haproxy1-internal"
 haproxy1_external="172.20.51.200 haproxy1-external"
 haproxy2_internal="192.168.51.101 haproxy2-internal"
 haproxy2_external="172.20.51.201 haproxy2-external"
+
 cluster_sql="192.168.51.110 cluster-sql"
 
 # Function to check and add entry to /etc/hosts
@@ -25,6 +28,14 @@ add_to_hosts() {
         echo "$name is already in /etc/hosts"
     fi
 }
+
+# Ensure 127.0.0.1 is properly configured for localhost
+echo "Configuring localhost entry in /etc/hosts..."
+if grep -q "127.0." /etc/hosts; then
+    sudo sed -i 's|^127\.0\..*|127.0.0.1 localhost|' /etc/hosts
+else
+    echo "127.0.0.1 localhost" | sudo tee -a /etc/hosts > /dev/null
+fi
 
 # Identify the current hostname
 hostname=$(hostname)
