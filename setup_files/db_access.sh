0872912f0423670a01c155453b87b60b18ed0027
David Santos <8220651@estg.ipp.pt>
2024-12-06 18:50:57 +0000

    changes

--- setup_files/db_access.sh
+++ setup_files/db_access.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#execute on sqls after cluster sql fully functional
+
 # Initial configuration
 echo "Configuring MariaDB/MySQL to allow external connections from web servers..."
 
@@ -8,9 +10,14 @@ DB_ROOT_PASS="root_password"
 HOST1="web1"
 HOST2="web2"
 
+
 echo "Granting privileges to specific hosts ($HOST1 and $HOST2)..."
 sudo mysql -u root -p$DB_ROOT_PASS -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'$HOST1' IDENTIFIED BY '$DB_ROOT_PASS' WITH GRANT OPTION;"
 sudo mysql -u root -p$DB_ROOT_PASS -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'$HOST2' IDENTIFIED BY '$DB_ROOT_PASS' WITH GRANT OPTION;"
+sudo mysql -u root -p$DB_ROOT_PASS -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'sql1' IDENTIFIED BY '$DB_ROOT_PASS' WITH GRANT OPTION;"
+sudo mysql -u root -p$DB_ROOT_PASS -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'sql2' IDENTIFIED BY '$DB_ROOT_PASS' WITH GRANT OPTION;"
+sudo mysql -u root -p$DB_ROOT_PASS -e "GRANT SELECT ON mysql.time_zone_name TO 'cactiuser'@'%';"
 sudo mysql -u root -p$DB_ROOT_PASS -e "FLUSH PRIVILEGES;"
+sudo mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -h 192.168.51.110 -u root -p mysql
 
 echo "Configuration complete! The SQL server now accepts connections from $HOST1 and $HOST2."
